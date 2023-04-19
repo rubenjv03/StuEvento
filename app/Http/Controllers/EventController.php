b81a915ebebc0efc7b7e32902315ca9cefe5a6ba
Rubén Jorge Verdeguer <st2021378@365.stucom.com>
2023-04-19 19:23:10 +0200

    functions defined

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function index(){
+
+    }
+    public function create(){
+        
+    }
+    public function store(){
+        
+    }
+    public function show($id){
+        
+    }
+    public function register($id){
+        
+    }
+    public function storeAttendee($idEvent){
+        
+    }
+    public function edit($id){
+        
+    }
+    public function update($id){
+        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/events',[EventController::class, 'index']);
+Route::get('/events/create',[EventController::class, 'create']);
+Route::get('/events/{id}',[EventController::class, 'show($id)']);
+Route::post('/events',[EventController::class, 'store']);
+Route::get('/events/{id}/register',[EventController::class, 'register($id)']);
+Route::post('/events/{id}/attendees',[EventController::class, 'storeAttendee($idEvent)']);
+Route::get('/events/{id}/edit',[EventController::class, 'edit($id)']);
+Route::put('/events/{id}',[EventController::class, 'update($id)']);
